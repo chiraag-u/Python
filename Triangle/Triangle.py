20414c9a1795b105ffcd227505d477f0a3143672
chiraag-u <76471280+chiraag-u@users.noreply.github.com>
2021-04-01 22:09:09 +0530

    Removed few Lines

--- Triangle/Triangle.py
+++ Triangle/Triangle.py
@@ -1,23 +1,23 @@
-print("\nTriangle Caculator\n\n    /|\n   / |\n  /  |\n /   |\n/____|\n\nWhat do you want to Calculate\nArea or Perimeter\n")
-Option = input("Tell Me ")
-if Option.capitalize() == "Area":
-    print("\nLets Calculate the Area of Triangle\n")
-    Base = input("Base: ")
-    Height = input("Height: ")
+print("\nArea and Perimeter of Triangle\n\n    /|\n   / |\n  /  |\n /   |\n/____|\n")
+Option = input("Do you want to calculate Area or Perimeter(A/P) ")
+if Option.capitalize() == "A":
+    print("\nLets Calculate the Area of the Triangle\n")
+    Base = input("Enter Base: ")
+    Height = input("Enter Height: ")
     try:            
         Area = int(Base) * int(Height) * .5
-        print("The Area is " + str(Area) + " sq.unit\n\nThankyou for using Triangle Calculator")
+        print("The Area of triangle is " + str(Area) + " sq.unit")
     except:
         print("Please Provide a Number")
-elif Option.capitalize() == "Perimeter":
+elif Option.capitalize() == "P":
     print("\nLets Calculate the Perimeter of Triangle\n")
-    Side1 = input("Firt Side: ")
-    Side2 = input("Second Side: ")
-    Side3 = input("Third Side: ")
+    Side1 = input("Enter the Firt Side: ")
+    Side2 = input("Enter the Second Side: ")
+    Side3 = input("Enter the Third Side: ")
     try:
         Perimeter = int(Side1) + int(Side2) + int(Side3)
-        print("The Perimeter is " + str(Perimeter) + " units\n\nThankyou for using Triangle Calculator")
+        print("The Perimeter of Triangle the is " + str(Perimeter) + " units")
     except:
         print("Please Provide a Number")
 else:
-    print("Sorry I did not understand\n")
+    print("Please Enter either Area or Perimeter\n")
