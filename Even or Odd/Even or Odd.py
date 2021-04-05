05193cf0670a7a3a6fea533e029b9075b6e4356b
Chiraag <76471280+chiraag-u@users.noreply.github.com>
2021-04-05 17:31:51 +0530

    A Code to Detect if a number is Even or Odd

---
+++ Even or Odd/Even or Odd.py
@@ -0,0 +1,10 @@
+print("\nEven Or Odd")
+print("A Code to Detect if a number is Even or Odd\n")
+Number = input("Enter the Number: ")
+try:
+    if int(Number) % 2 == 0:
+        print(str(Number) + " is an Even Number")
+    else:
+        print(str(Number) + " is an Odd Number")
+except:
+    print("\nEnter a Number")
