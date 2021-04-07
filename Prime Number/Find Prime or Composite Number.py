36519734fd8d42534f787d96ed9f6f6d16de92c7
Chiraag <76471280+chiraag-u@users.noreply.github.com>
2021-04-07 23:26:29 +0530

    Find Prime or Composite Number

--- Prime Number/Find Prime or Composite Number.py
+++ Prime Number/Find Prime or Composite Number.py
@@ -2,6 +2,7 @@ def IsPrime(n):
 	if (n <= 1):
 		return False
 	for i in range(2, n):
+		print(i)
 		if (n % i == 0):
 			return False
 	return True
@@ -9,6 +10,6 @@ def IsPrime(n):
 Number = input("Enter a Number: ")
 
 if IsPrime(int(Number)):
-	print("True")
+	print(str(Number) + " is a Prime Number")
 else:
-	print("False")
+	print(str(Number) + " is a Composite Number")
